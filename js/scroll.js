b7689dfd8b6a6cb97cbe307a180cf73d69f5936b
sasha <e-finder@rambler.ru>
2016-04-19 21:37:05 +0300

    cancelling fixed position after scrolling to footer
    ok

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,24 @@
 ﻿window.onload=function(){
 	var menu=document.getElementById('upper-menu');
+	var footerPosition=document.getElementById('footer').offsetTop;
 	var menuPosition=menu.offsetTop;
+	var menuHeight=menu.clientHeight;
+	
 	window.onscroll=function(){
 	
-	var offset=window.pageYOffset;
-	
-	if (offset>menuPosition){
-			menu.style.position='fixed';
-		}
-	else{
+		var offset=window.pageYOffset;
+		if (offset<menuPosition){
 			menu.style.position='static';
-		}
+			
+			}
+		else if (offset<footerPosition-menuHeight){
+			menu.style.position='fixed';
+			menu.style.top='0';
+			}
+		else{
+			menu.style.position='absolute';
+			menu.style.top=(footerPosition-menuHeight)+'px';
+			
+			}
 	}
 }

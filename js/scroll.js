b78fd6207d43a54a5fc4c7e16b8d78724db4ff4c
sasha <e-finder@rambler.ru>
2016-04-19 20:37:42 +0300

    initial
    commit

---
+++ js/scroll.js
@@ -0,0 +1,15 @@
+﻿window.onload=function(){
+	var menu=document.getElementById('upper-menu');
+	var menuPosition=menu.offsetTop;
+	window.onscroll=function(){
+	
+	var offset=window.pageYOffset;
+	
+	if (offset>menuPosition){
+			menu.style.position='fixed';
+		}
+	else{
+			menu.style.position='static';
+		}
+	}
+}
